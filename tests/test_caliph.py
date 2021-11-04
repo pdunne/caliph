7271177c55f7a0919382e44978183d6e971cba7c
Peter Dunne <peter.dunne@applied-magnetism.com>
2021-11-04 17:42:46 +0100

    First commit
    A simple tool to calibrate and convert pH measurements using a two point method.

---
+++ tests/test_caliph.py
@@ -0,0 +1,16 @@
+from caliph import __version__, routines
+import numpy as np
+
+
+def test_version():
+    assert __version__ == "0.1.0"
+
+
+def test_calib():
+    result = routines.pH_calibration([3.75, 9.49], 21.0)
+    assert np.allclose(result, [1.05365854, 0.05078049])
+
+
+def test_conv():
+    result = routines.pH_convert(3.5, [1.05365854, 0.05078049])
+    assert np.allclose(result, 3.738585365853657)
